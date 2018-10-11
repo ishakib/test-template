ed9689cbd0c1739075361013c66f75dd5345f6ea
unknown <akm.shakib@gmail.com>
2018-10-11 13:05:09 +0600

    Skeletonof site

---
+++ js/custom-owl-carousel.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:500,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:2,
+            nav:true,
+         
+        }
+    }
+})
